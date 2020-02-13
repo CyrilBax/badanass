2ec74d0aed5be9cb30206ed25e3df943ce77d094
cbaxiu <cbaxiu@gmail.com>
2020-02-13 18:13:14 +0100

    feat(navigate): add navgation to module user

--- app/src/main/java/com/example/badanass/MainActivity.kt
+++ app/src/main/java/com/example/badanass/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.badanass
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import com.example.badanass.detail.CardDetailFragment
 import com.example.badanass.list.CardListFragment
+import com.example.user.UserActivity
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity(), MainActivityContract {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar_main)
 
         supportFragmentManager.beginTransaction().run {
             replace(R.id.activity_main, CardListFragment.newInstance())
@@ -34,5 +42,25 @@ class MainActivity : AppCompatActivity(), MainActivityContract {
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
         super.onBackPressed()
         return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_button -> navigateModule()
+            else -> super.onOptionsItemSelected(item)
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun navigateModule() {
+        val intent = Intent(this, UserActivity::class.java)
+        startActivity(intent)
     }
 }
