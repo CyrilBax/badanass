8c8fed20f7e1210e716c5af5e623e4d2ef2fcca5
cbaxiu <cbaxiu@gmail.com>
2020-02-13 15:12:14 +0100

    feat(module): create module user

---
+++ user/src/main/java/com/example/user/UserActivity.kt
@@ -0,0 +1,19 @@
+package com.example.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.user.ui.user.UserFragment
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.user_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, UserFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
