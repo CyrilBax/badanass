3e1fa21a5d6145af400c8c862cd39c2d9245275d
cbaxiu <cbaxiu@gmail.com>
2020-02-13 18:38:11 +0100

    feat(toolbar): introduce toolbar into user module

--- user/src/main/java/com/example/user/UserActivity.kt
+++ user/src/main/java/com/example/user/UserActivity.kt
@@ -2,18 +2,31 @@ package com.example.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import com.example.user.ui.user.UserFragment
+import kotlinx.android.synthetic.main.toolbar.*
 
 class UserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.user_activity)
+
+//        setSupportActionBar(toolbar_user)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, UserFragment.newInstance())
+                .replace(R.id.user_activity, UserFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        return true
     }
 
 }
